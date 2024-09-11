b512e8053ce9fa33dffc96d806bc1babd9d4abcf
jishnu m <mjishnu990@gmail.com>
2024-09-11 13:59:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://twitter-clone-backend-v3rv.onrender.com', // Replace with your API server
+        target: ' https://twitter-clone-backend-v3rv.onrender.com', // Replace with your API server
         changeOrigin: true,
         secure: false,
       },
